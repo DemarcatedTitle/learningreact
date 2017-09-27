d63f451244014286e31d20ad5af981c34de7fe2c
dt <dimmton@gmail.com>
2017-09-26 22:21:18 -0700

    Add beginnings of Game History page

---
+++ walkgrid/src/GameHistory.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+import React from "react";
+// import Rooms from "./Rooms.js";
+// import Users from "./Users.js";
+// import moment from "moment";
+class GameHistory extends React.PureComponent {
+    constructor(props) {
+        super(props);
+    }
+    handleSubmit(event) {}
+    handleChange(event) {}
+    componentDidUpdate() {}
+    componentWillUnmount() {}
+    render() {
+        return (
+            <div>
+                Placeholder text.
+            </div>
+        );
+    }
+}
+export default GameHistory;
