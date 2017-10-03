d646b573d2cbb35813355f2bf8c6a32a4e7d0df9
dt <dimmton@gmail.com>
2017-10-02 23:08:12 -0700

    New Feature: Waiting Room

--- api/gamestate.js
+++ api/gamestate.js
@@ -2,5 +2,14 @@ exports.chatters = new Map();
 exports.chatlogs = new Map();
 exports.rooms = new Map();
 exports.games = new Map();
+// Individual game state will be kept here
+// It will contain:
+// "coords" list of lists
+// "occupied" list of lists
+// "grid" array of arrays
+// "players" Map
+// a "collision" object
+// an "outcome" string.
+
 exports.activePlayers = new Map();
 exports.gameHistory = [];
