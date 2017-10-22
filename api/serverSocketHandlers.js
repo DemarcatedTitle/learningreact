2dc364324909a28b25848bc789668911ef09ad30
dt <dimmton@gmail.com>
2017-10-22 14:29:43 -0700

    Change chat message callback to be its own file

--- api/serverSocketHandlers.js
+++ api/serverSocketHandlers.js
@@ -5,6 +5,7 @@ const keypress = require("./keypress.js").keypress;
 const gameInit = require("./gameInit.js").gameInit;
 const moveSquare = require("./grid/stateChanges.js").moveSquare;
 const join = require("./join").join;
+const chatmessage = require("./chatmessage").chatmessage;
 const newgame = require("./newgame").newgame;
 const {
     chatters,
@@ -77,16 +78,6 @@ function forfeitPrevious(username, socket, currentRoom, reason) {
     }
 }
 
-function updateRoomLists() {
-    // Iterate over each room
-    io.in(currentRoom).emit(
-        "rooms",
-        JSON.stringify({
-            rooms: Array.from(rooms.keys())
-            // currentRoom: currentRoom
-        })
-    );
-}
 exports.io = function(listener, secret, users) {
     io = require("socket.io")(listener);
 
@@ -141,6 +132,8 @@ exports.io = function(listener, secret, users) {
             });
         });
         const socketContext = {
+            chatMessageEmission: chatMessageEmission,
+            getCurrentRoom: getCurrentRoom,
             JWT: JWT,
             userToken: userToken,
             secret: secret,
@@ -169,196 +162,7 @@ exports.io = function(listener, secret, users) {
             chatters.delete(socket.id);
         });
         socket.on("new room", newgame.bind(socketContext));
-        // function(room) {
-        //     currentRoom = room;
-        //     JWT.verify(userToken, secret, function(err, decoded) {
-        //         if (err) {
-        //             io.emit("error", "Something went wrong");
-        //         } else if (decoded.username) {
-        //             // Recursively find a number to append to room name to allow people to have an arbitrary number of rooms
-        //             function roomN(room, rooms, digit) {
-        //                 if (rooms.has(`${room}-${digit}`) === true) {
-        //                     return roomN(room, rooms, digit + 1);
-        //                 } else {
-        //                     return `${room}-${digit}`;
-        //                 }
-        //             }
-        //             let i = 2;
-        //             if (rooms.has(room) === false) {
-        //                 forfeitPrevious(decoded.username, socket, currentRoom);
-        //                 rooms.set(room, io.of(room));
-        //                 chatlogs.set(currentRoom, []);
-        //                 let grid = [];
-        //                 const state = gameInit();
-        //                 state.players = new Map([[decoded.username, 0]]);
-        //                 games.set(currentRoom, state);
-        //                 socket.join(room, () => {
-        //                     socket.broadcast.emit(
-        //                         "rooms",
-        //                         JSON.stringify({
-        //                             rooms: Array.from(rooms.keys())
-        //                         })
-        //                     );
-        //                     io.in(currentRoom).clients((error, clients) => {
-        //                         if (error) throw error;
-        //                         io.to(socket.id).emit(
-        //                             "users",
-        //                             JSON.stringify({
-        //                                 users: clients.map(client =>
-        //                                     chatters.get(client)
-        //                                 ),
-        //                                 currentUser: decoded.username
-        //                             })
-        //                         );
-        //                     });
-
-        //                     io.to(socket.id).emit(
-        //                         "rooms",
-        //                         JSON.stringify({
-        //                             rooms: Array.from(rooms.keys()),
-        //                             currentRoom: room
-        //                         })
-        //                     );
-        //                     gameUpdates(socket, state, decoded.username, room);
-        //                     activePlayers.set(decoded.username, currentRoom);
-        //                 });
-        //                 let context = {
-        //                     room,
-        //                     username: decoded.username,
-        //                     io,
-        //                     socket
-        //                 };
-        //                 socket.removeAllListeners("keypress");
-        //                 socket.on("keypress", keypress.bind(context));
-        //             } else {
-        //                 let numberedRoom = roomN(room, rooms, i);
-        //                 forfeitPrevious(decoded.username, socket, numberedRoom);
-        //                 rooms.set(numberedRoom, io.of(numberedRoom));
-        //                 chatlogs.set(numberedRoom, []);
-        //                 let grid = [];
-        //                 const state = gameInit();
-        //                 state.players = new Map([[decoded.username, 0]]);
-        //                 games.set(numberedRoom, state);
-        //                 socket.join(numberedRoom, () => {
-        //                     socket.broadcast.emit(
-        //                         "rooms",
-        //                         JSON.stringify({
-        //                             rooms: Array.from(rooms.keys())
-        //                         })
-        //                     );
-        //                     io.in(numberedRoom).clients((error, clients) => {
-        //                         if (error) throw error;
-        //                         io.to(socket.id).emit(
-        //                             "users",
-        //                             JSON.stringify({
-        //                                 users: clients.map(client =>
-        //                                     chatters.get(client)
-        //                                 ),
-        //                                 currentUser: decoded.username
-        //                             })
-        //                         );
-        //                     });
-
-        //                     io.to(socket.id).emit(
-        //                         "rooms",
-        //                         JSON.stringify({
-        //                             rooms: Array.from(rooms.keys()),
-        //                             currentRoom: numberedRoom
-        //                         })
-        //                     );
-        //                     gameUpdates(
-        //                         socket,
-        //                         state,
-        //                         decoded.username,
-        //                         numberedRoom
-        //                     );
-        //                     activePlayers.set(decoded.username, numberedRoom);
-        //                 });
-        //                 let context = {
-        //                     room: numberedRoom,
-        //                     username: decoded.username,
-        //                     io,
-        //                     socket
-        //                 };
-        //                 socket.removeAllListeners("keypress");
-        //                 socket.on("keypress", keypress.bind(context));
-        //             }
-        //         }
-        //     });
-        // });
-        JWT.verify(userToken, secret, function(err, decoded) {
-            if (err) {
-                socket.emit("error", "Something went wrong");
-            } else {
-                socket.on("chat message", function(message) {
-                    function chatCommands(command) {
-                        let reply;
-                        const rooms = ["Tutorial"];
-                        if (command === "/tutorial") {
-                            console.log(rooms);
-                            forfeitPrevious(
-                                decoded.username,
-                                socket,
-                                getCurrentRoom(
-                                    socket.id,
-                                    activePlayers,
-                                    chatters
-                                ),
-                                "tutorial"
-                            );
-                            io.to(socket.id).emit(
-                                "rooms",
-                                JSON.stringify({
-                                    rooms: rooms,
-                                    currentRoom: "Tutorial"
-                                })
-                            );
-
-                            reply = JSON.stringify({
-                                room: "tutorial",
-                                logs: [
-                                    {
-                                        date: new Date(),
-                                        message:
-                                            "You just asked for a tutorial",
-                                        username: "Server"
-                                    }
-                                ]
-                            });
-                            io.to(socket.id).emit("chat message", reply);
-                        }
-                        return reply;
-                    }
-                    if (chatlogs.get(currentRoom)) {
-                        console.log(`${chatters.get(socket.id)} just spoke`);
-                        io.in(currentRoom).clients((error, clients) => {
-                            if (error) throw error;
-                            console.log(
-                                `The following just heard:
-                    ${clients.map(function(id) {
-                        return chatters.get(id);
-                    })}
-                    `
-                            );
-                        });
-                        console.log(`room is ${currentRoom}`);
-                        chatlogs.get(currentRoom).push({
-                            date: new Date(),
-                            message: message,
-                            username: decoded.username
-                        });
-                        io
-                            .in(currentRoom)
-                            .emit(
-                                "chat message",
-                                chatMessageEmission(currentRoom, chatlogs)
-                            );
-                    }
-                    chatCommands(message);
-                });
-            }
-        });
-
+        socket.on("chat message", chatmessage.bind(socketContext));
         socket.on("join", join.bind(socketContext));
         socket.on("gameHistory", function(request) {
             console.log("gameHistory requested");
